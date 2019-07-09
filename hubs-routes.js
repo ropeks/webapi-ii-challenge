4002c50d545bf7bdcddd3fabb244ea36cd09dcda
ropeks <ropeksmilioner@gmail.com>
2019-07-09 15:35:14 +0200

    define post comment request

--- hubs-routes.js
+++ hubs-routes.js
@@ -13,12 +13,38 @@ route.post('/', async (req, res) => {
             res.status(500).json({ 
                 error: "There was an error while saving the post to the database" 
             });
-        };
+        }
     } else {
         res.status(400).json({ 
             errorMessage: "Please provide title and contents for the post." 
         });
     }
 });
+
+route.post('/:id/comments', async (req, res) => {
+    const { id } = req.params;
+    const { text } = req.body;
+    const commentInfo = { ...req.body, post_id: id };
+    // const post = await db.findById(id);
+    // if (!post) {
+    //     res.status(404).json({
+    //         message: "The post with the specified ID does not exist."
+    //     });
+    // } else 
+    if (!text) {
+        res.status(400).json({
+            errorMessage: "Please provide text for the comment."
+        });
+    } else {
+        try {
+            const comment = await db.insertComment(commentInfo);
+            res.status(201).json(comment);
+        } catch {
+            res.status(500).json({
+                error: "There was an error while saving the comment to the database"
+            });
+        }
+    }
+})
 
 module.exports = route;
