de7cde0902aa9e38da83ac6ac223a3b9d3b11784
ropeks <ropeksmilioner@gmail.com>
2019-07-09 15:47:20 +0200

    define get and getById requests

--- hubs-routes.js
+++ hubs-routes.js
@@ -9,7 +9,6 @@ route.post('/', async (req, res) => {
             const posts = await db.insert(req.body);
             res.status(201).json(posts);
         } catch (error) {
-            console.log(error);
             res.status(500).json({ 
                 error: "There was an error while saving the post to the database" 
             });
@@ -44,6 +43,34 @@ route.post('/:id/comments', async (req, res) => {
                 error: "There was an error while saving the comment to the database"
             });
         }
+    }
+})
+
+route.get('/', async (req, res) => {
+    try {
+        const posts = await db.find();
+        res.status(200).json(posts);
+    } catch {
+        res.status(500).json({
+            error: "The posts information could not be retrieved."
+        });
+    }
+})
+
+route.get('/:id', async (req, res) => {
+    try {
+        const post = await db.findById(req.params.id);
+        if (post) {
+           res.status(200).json(post); 
+        } else {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist."
+            })
+        }
+    } catch {
+        res.status(500).json({
+            error: "The posts information could not be retrieved."
+        });
     }
 })
 
