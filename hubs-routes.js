91dd1f273b6fc3eb98296b17b98c2d47f6da905a
ropeks <ropeksmilioner@gmail.com>
2019-07-09 16:25:49 +0200

    define put request

--- hubs-routes.js
+++ hubs-routes.js
@@ -113,4 +113,28 @@ route.delete('/:id', async (req, res) => {
     }
 });
 
+route.put('/:id', async (req, res) => {
+    try {
+        const { title, contents } = req.body;
+        const post = await db.update(req.params.id, req.body);
+        if (post) {
+            if ( title && contents ) {
+                res.status(200).json(post);
+            } else {
+                res.status(400).json({
+                    errorMessage: "Please provide title and contents for the post."
+                });
+            }
+        } else {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist."
+            });
+        }
+    } catch {
+        res.status(500).json({
+            error: "The post information could not be modified."
+        });
+    }
+});
+
 module.exports = route;
